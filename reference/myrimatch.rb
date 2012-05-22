c268525502e1980e1152cec292953585c2b2c7f7
John Prince <jtprince@gmail.com>
2012-05-22 16:26:01 -0600

    beginning reference section

---
+++ reference/myrimatch.rb
@@ -0,0 +1,15 @@
+
+module Corymb
+  module Preprocess
+    module Myrimatch
+      
+      # works on a Spectrum object
+      def classify_by_intensity(levels = [0.25 0.5, 0.75])
+      end
+
+
+      def noise_removal
+      end
+    end
+  end
+end
